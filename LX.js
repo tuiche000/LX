7386f00991fffe6dd5074e8a255f69c95f96386d
tuiche000 <845042291@qq.com>
2017-07-11 23:16:36 +0800

    下拉框用jquery完成的差不太多了

--- LX.js
+++ LX.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-/**
- * Created by lx on 2017/7/10.
- */
-
-/*
-    Ajax对象
-    url:文件地址
-    suc:响应成功回调函数
-    err:响应失败回调函数
- */
-function Ajax(url,suc,err){
-    //创建XMLHttpRequest对象
-    var xmlhttp;
-    if(window.XMLHttpRequest){
-        xmlhttp = new XMLHttpRequest();
-    }else {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    //请求
-    xmlhttp.open('POST',url + "?t=" + new Date().getTime(),true);
-    xmlhttp.send();
-    xmlhttp.onreadystatechange=function()
-    {
-        if (xmlhttp.readyState==4)
-        {
-            if(xmlhttp.status==200){
-                res = xmlhttp.responseText;
-                suc(res);
-            }else {
-                if(err){
-                    err(xmlhttp.status);
-                }
-            }
-        }
-    }
-}
-
-/*
-    下拉框
- */
-=======
 /**
  * Created by lx on 2017/7/10.
  */
@@ -81,14 +39,63 @@ function Ajax(url,suc,err){
     下拉框
  */
 
-function DropDown(id,sanjiao){
+function DropDown(){
     var sj = $(".sanjiao");
-    var box = $("#ul");
-    var ulShow;
+    var input = $("#input");
+    var ul = $("#ul");
+    var li = ul.find('li');
     sj.click(function(){
-       alert('111')
+        ul.slideToggle(0);
+        li.removeClass('active')
     });
+    input.on('input',function(){
+        var v = $(this).val();
+        var patt = new RegExp(v);
+        for(var i=0; i<li.length; i++){
+            var val = li.eq(i).text();
+            console.log(val);
+            if(patt.test(val)){
+                li.eq(i).show().siblings().hide();
+                if(v == null || v== ""){
+                    li.show();
+                }
+            }
+        }
+    });
+
+
+    var index1;
+    //键盘按下
+    input[0].onkeydown=function(e){
+        var key = e || event;
+        if(key.keyCode == 40){
+            if(!index1 && index1!=0){
+                index1= -1;
+            }
+            if(index1 < li.length-1){
+                index1++;
+            }
+            console.log(index1);
+
+            li.eq(index1).addClass('active').siblings().removeClass('active');
+        }
+        if(key.keyCode == 38){
+            if(index1 > 0){
+                index1--;
+            }
+            console.log(index1);
+
+            li.eq(index1).addClass('active').siblings().removeClass('active');
+        }
+        if(key.keyCode == 13){
+            var val = $(".active").text();
+            console.log(val)
+            console.log(input.text())
+            input.val(val);
+        }
+    }
+
+
 
 }
 
->>>>>>> cc61698b7f8d4b4809ac17b22495c5e879e1e7fe
